07447d07ef7e3dd67580a3f43dee9d1d11096512
Franlish3 <mutanganzanzu@gmail.com>
2025-04-03 13:24:09 +0200

    add

--- TP2/Program.cs
+++ TP2/Program.cs
@@ -19,7 +19,7 @@ namespace TP2
 
             Console.WriteLine("ABSTRACT CLASS");
             Console.WriteLine("===============\n");
-            Teacher t1 = new Teacher("frank", "nzanzu", "jife788");
+            Teacher t1 = new Teacher("frank", "nzanzu", "jiggggdfe788");
             Student s1 = new Student("james", "jos", "22lsi65774");
 
             t1.Add(2);
